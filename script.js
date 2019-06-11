ef70135cfb3d33c714a02463bb93ed5453dd4d80
infiniti008 <nikitenok.sl@gmail.com>
2019-06-11 09:03:04 +0300

    Add PWA files

--- script.js
+++ script.js
@@ -129,3 +129,12 @@ function writeTime (h, m, s){
   document.getElementById('mm').innerHTML = m;
   document.getElementById('ss').innerHTML = s;
 }
+
+
+
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+    });
+}
